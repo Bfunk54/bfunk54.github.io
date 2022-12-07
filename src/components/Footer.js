dc0a9bed7c48e477e29915dc073896549c6ef359
Bfunk54 <ben_fein@icloud.com>
2022-12-07 18:37:40 -0500

    finished footer!

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,18 +3,18 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="footer">
-        <span>Ben's Portfolio 2022</span>
+        <span style={{fontSize: '16px'}}>Ben's Portfolio 2022 </span>
 
-        <a style= {{padding: '10px'}} href="https://github.com/Bfunk54" target="_blank" rel="noreferrer">
-            <i className="bi bi-github"></i>
+        <a style= {{padding: '16px'}} href="https://github.com/Bfunk54" target="_blank" rel="noreferrer">
+            <i style={{fontSize: '40px'}} className="bi bi-github"><span style={{fontSize: '10px'}}>github</span></i>
         </a>
 
-        <a style= {{padding: '10px'}} href="https://www.linkedin.com/in/benjamin-fein-5b0b1b1b3/" target="_blank" rel="noreferrer">
-            <i className="bi bi-linkedin"></i>
+        <a style= {{padding: '16px'}} href="https://www.linkedin.com/in/benjamin-fein-5a73b2242/" target="_blank" rel="noreferrer">
+            <i style={{fontSize: '40px'}} className="bi bi-linkedin"><span style={{fontSize: '10px', padding: '4px'}}>linkedin</span></i>
         </a>
 
-        <a style= {{padding: '10px'}} href="https://twitter.com/ben_fein" target="_blank" rel="noreferrer">
-            <i className="bi bi-twitter"></i>
+        <a style= {{padding: '16px'}} href="https://twitter.com/ben_fein" target="_blank" rel="noreferrer">
+            <i style={{fontSize: '40px'}} className="bi bi-twitter"><span style={{fontSize: '10px'}}>twitter</span></i>
         </a>
     </footer>
     )};
