c77f19dd53c5f28d0a42fd20fcb68ef3eea6d6f0
Bfunk54 <ben_fein@icloud.com>
2022-12-07 18:56:41 -0500

    finished styling the homepage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -10,12 +10,12 @@ export default function Home() {
             <div className="container">
                 <div className="avatar" ></div>
                 <div className="about-me">
-                    <p>A full stack web developer</p><a className="btn btn-outline-primary" role="button" href="#">More About Me</a>
+                    <p>A full stack web developer</p><a className="btn btn-outline-primary btn-sm" role="button" href="#">Full Portfolio</a>
                 </div>
             </div>
         </section>
         <section className="portfolio-block photography">
-            <div className="container">
+            <div style= {{padding: '20px'}} className="container">
                 <div className="row g-0">
                     <div className="col-md-6 col-lg-4 item zoom-on-hover"><a href="#"><img className="img-fluid image" src={nature5}></img></a></div>
                     <div className="col-md-6 col-lg-4 item zoom-on-hover"><a href="#"><img className="img-fluid image" src={nature2}></img></a></div>
@@ -25,15 +25,51 @@ export default function Home() {
         </section>
         <section className="portfolio-block call-to-action border-bottom">
             <div className="container">
-                <div className="d-flex justify-content-center align-items-center content">
-                    <h3>Like what you see?</h3><button className="btn btn-outline-primary btn-lg" type="button">Contact me</button>
+                <div style= {{paddingBottom: '20px'}} className="d-flex justify-content-center align-items-center content">
+                    <h3 style= {{paddingRight: '20px'}}>Like what you see?</h3><button className="btn btn-outline-primary btn-lg" type="button">Contact me</button>
                 </div>
             </div>
         </section>
-        <section className="portfolio-block skills">
-            <div className="container">
+        <section className="portfolio-block skills border-bottom">
+            <div style= {{paddingTop: '20px'}} className="container">
                 <div className="heading">
                     <h2>Special Skills</h2>
+                </div>
+                <div className="row">
+                    <div className="col-md-4">
+                        <div className="card special-skill-item border-0">
+                            <div className="card-header bg-transparent border-0"><i className="icon ion-ios-star-outline"></i></div>
+                            <div className="card-body">
+                                <h3 className="card-title">full stack apps</h3>
+                                <p className="card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="card special-skill-item border-0">
+                            <div className="card-header bg-transparent border-0"><i className="icon ion-ios-lightbulb-outline"></i></div>
+                            <div className="card-body">
+                                <h3 className="card-title">web apps</h3>
+                                <p className="card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="card special-skill-item border-0">
+                            <div className="card-header bg-transparent border-0"><i className="icon ion-ios-gear-outline"></i></div>
+                            <div className="card-body">
+                                <h3 className="card-title">back-end server developement</h3>
+                                <p className="card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section className="portfolio-block technologies">
+            <div style= {{paddingTop: '20px'}} className="container">
+                <div className="heading">
+                    <h2>Technologies I've Used</h2>
                 </div>
                 <div className="row">
                     <div className="col-md-4">
