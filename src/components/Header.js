ee5ce312fbbf6d732542ca61136c414ef0c4876e
Bfunk54 <ben_fein@icloud.com>
2022-12-19 10:08:02 -0500

    added new project

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Collapse from 'react-bootstrap/Collapse';
 
+{/* <Hamburger toggled={isOpen} toggle={setOpen} duration={0.8} rounded size={20} color="#696eff" /> */}
 
 function Nav({ currentPage, handlePageChange }) {
   return (
     // 696eff  -- navbar color
-    <nav className="navbar bg-dark bg-gradient navbar-dark navbar-expand-lg fixed-top bg-primary portfolio-navbar gradient">
+    <nav style= {{backgroundColor: 'rgba(245, 245, 245, 0.4)'}} className="navbar navbar-dark navbar-expand-lg fixed-top portfolio-navbar gradient">
         <div className="container"><Collapse in={true} appear={true} dimension="width" timeout={9999}><a className="navbar-brand logo" style={{fontSize: '28px'}} href="#">Benjamin Fein</a></Collapse><button data-bs-toggle="collapse" className="navbar-toggler" data-bs-target="#navbarNav"><span className="visually-hidden">Toggle navigation</span><span className="navbar-toggler-icon"></span></button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav ms-auto">
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import { apply as applySpring, useSpring, a, interpolater } from 'react-spring'
+import { extend as applyThree, Canvas, useFrame, useThree } from 'react-three-fiber'
+import React, { useState, useRef, useEffect, useCallback, useMemo } from 'react'
 import nature1 from '../img/nature/image1.jpg';
 import nature2 from '../img/nature/image2.jpg';
 import nature3 from '../img/nature/image3.jpg';
@@ -8,8 +10,34 @@ import nature6 from '../img/nature/image6.jpg';
 import nature7 from '../img/nature/image7.jpg';
 import nature8 from '../img/nature/image8.jpg';
 import nature9 from '../img/nature/image9.jpg';
+import USPostalPrinting from '../img/portfolio/USPostalPrinting.png';
+
 
 export default function Portfolio() {
+
+    function Background({ color }) {
+        const { viewport } = useThree()
+        return (
+          <mesh scale={[viewport.width, viewport.height, 1]}>
+            <planeGeometry attach="geometry" args={[1, 1]} />
+            <a.meshBasicMaterial attach="material" color={color} depthTest={false} />
+          </mesh>
+        )
+      }
+
+    const Effects = React.memo(({ factor }) => {
+        const { gl, scene, camera, size } = useThree()
+        const composer = useRef()
+        useEffect(() => void composer.current.setSize(size.width, size.height), [size])
+        // This takes over as the main render-loop (when 2nd arg is set to true)
+        useFrame(() => composer.current.render(), 1)
+        return (
+          <effectComposer ref={composer} args={[gl]}>
+            <renderPass attachArray="passes" args={[scene, camera]} />
+            <a.glitchPass attachArray="passes" renderToScreen factor={factor} />
+          </effectComposer>
+        )
+      })
   return (
     <div>
       <section className="portfolio-block projects-with-sidebar">
@@ -29,7 +57,7 @@ export default function Portfolio() {
                     </div>
                     <div className="col-md-9">
                         <div className="row">
-                            <div className="col-md-6 col-lg-4 project-sidebar-card"><a href="#"><img className="img-fluid image scale-on-hover" src={nature1}></img></a></div>
+                            <div className="col-md-6 col-lg-4 project-sidebar-card"><a href="https://us-postal-printing.herokuapp.com/"><img className="img-fluid image scale-on-hover" src={USPostalPrinting}></img></a></div>
                             <div className="col-md-6 col-lg-4 project-sidebar-card"><a href="#"><img className="img-fluid image scale-on-hover" src={nature2}></img></a></div>
                             <div className="col-md-6 col-lg-4 project-sidebar-card"><a href="#"><img className="img-fluid image scale-on-hover" src={nature3}></img></a></div>
                             <div className="col-md-6 col-lg-4 project-sidebar-card"><a href="#"><img className="img-fluid image scale-on-hover" src={nature4}></img></a></div>
