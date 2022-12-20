3137c834be18251ff58f60015571710cc667afb4
Bfunk54 <ben_fein@icloud.com>
2022-12-20 07:44:16 -0500

    button fix on contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -38,7 +38,7 @@ export default function Contact() {
             <div className="mb-3"><label className="form-label" htmlFor="subject">Subject</label><input className="form-control item" type="text" id="subject"></input></div>
             <div className="mb-3"><label className="form-label" htmlFor="email">Email</label><input className="form-control item" type="email" id="email"></input></div>
             <div className="mb-3"><label className="form-label" htmlFor="message">Message</label><textarea className="form-control item" id="message"></textarea></div>
-            <div className="mb-3"><button className="btn btn-primary btn-lg d-block w-100" type="submit">Submit Form</button></div>
+            <div className="mb-3"><button className="btn btn-bd-primary btn" style={{width: '100%'}} type="submit">Submit Form</button></div>
         </form>
     </div>
 </section>
