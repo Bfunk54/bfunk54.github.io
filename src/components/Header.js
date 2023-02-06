27643f9f431a6c65aafa0104e6ec7f7fbb76b419
Bfunk54 <ben_fein@icloud.com>
2023-02-06 18:24:09 -0500

    finally, after many hours, got the mobile menu to close when a menu button is pressed!!!!

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,64 +4,67 @@ import './pages/Home.css'
 import Logo from './img/home/BenjaminFeinLogo.png'
 import './Header.css'
 import Hamburger from "hamburger-react";
+import { Link } from "react-router-dom";
 
 {/* <Hamburger toggled={isOpen} toggle={setOpen} duration={0.8} rounded size={20} color="#696eff" /> */}
 
 function Nav({ currentPage, handlePageChange }) {
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
   return (
     // 696eff  -- navbar color
     <nav style= {{backgroundColor: 'rgba(245, 245, 245, 0.7)', borderBottomLeftRadius: "40px", borderBottomRightRadius: "40px" }} className="navbar navbar-dark navbar-expand-lg fixed-top portfolio-navbar gradient">
-        <div style={{display: 'flex', justifyContent: 'center'}} className="container"><Collapse in={true} appear={true} dimension="width" timeout={9999}><a className="navbar-brand logo" style={{fontSize: '34px', color: 'black', fontWeight: 500}} href="#"><img style={{height: 110, width: 525}} className='logo' src={Logo} /></a></Collapse><button style={{marginTop: '-25px'}} data-bs-toggle="collapse" className="navbar-toggler" data-bs-target="#navbarNav"><span className="visually-hidden">Toggle navigation</span><Hamburger color="#4FD1C5" rounded={true} style={{justifyContent: 'center'}} className="navbar-toggler-icon"></Hamburger></button>
+        <div style={{display: 'flex', justifyContent: 'center'}} className="container"><Collapse in={true} appear={open} onEnter={toggleDrawer(true)} onExit={toggleDrawer(false)} dimension="width" timeout={9999}><a className="navbar-brand logo" style={{fontSize: '34px', color: 'black', fontWeight: 500}} href="#"><img style={{height: 110, width: 525}} className='logo' src={Logo} /></a></Collapse><button style={{marginTop: '-25px', textDecoration: "none"}} data-bs-toggle="collapse" className="navbar-toggler" data-bs-target="#navbarNav"><span className="visually-hidden">Toggle navigation</span><Hamburger id="nav" onToggle={toggleDrawer(true)} color="#4FD1C5" rounded={true} style={{justifyContent: 'center'}} className="navbar-toggler-icon"/></button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul style={{display: 'flex', justifyContent: 'center'}} className="navbar-nav ms-auto">
                 <li style={{display: 'flex', justifyContent: 'center', marginRight: '10px'}} className="nav-item home">
-                <button className="btn btn-bd-primary btn" type="button">
-                <a
-          href="#home"
-          onClick={() => handlePageChange('Home')}
-
-          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
-        >
+                <Link to="/" style={{ color: "white", textDecoration: "none", fontSize: 18 }}
+                onClick={() => handlePageChange('Home') }
+                className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+                >
+                  <button className="btn btn-bd-primary btn navbar-toggler"
+                  data-bs-toggle="collapse" data-bs-target="#navbarNav" type="button">
           Home
-        </a>
-        </button>
+          </button>
+        </Link>
       </li>
       <li style={{display: 'flex', justifyContent: 'center', marginRight: '10px'}} className="nav-item">
       <button className="btn btn-bd-primary btn" type="button">
+      <Link to="/portfolio" style={{ color: "white", textDecoration: "none", fontSize: 18 }}>
         <a
-          href="#portfolio"
-          onClick={() => handlePageChange('Portfolio')}
-
-          className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
-          
+        onClick={() => { handlePageChange('Portfolio'); toggleDrawer(false)} }
+        className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
         >
           Portfolio
         </a>
+        </Link>
         </button>
       </li>
       <li style={{display: 'flex', justifyContent: 'center', marginRight: '10px'}} className="nav-item">
       <button className="btn btn-bd-primary btn" type="button">
+      <Link to="/resume" style={{ color: "white", textDecoration: "none", fontSize: 18 }}>
         <a
-          href="#resume"
-          onClick={() => handlePageChange('Resume')}
-
-          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-          
+        onClick={() => { handlePageChange('Resume'); toggleDrawer(false)} }
+        className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
         >
           Resume
         </a>
+        </Link>
         </button>
       </li>
       <li style={{display: 'flex', justifyContent: 'center'}} className="nav-item contact">
       <button className="btn btn-bd-primary btn" type="button">
+      <Link to="/contact" style={{ color: "white", textDecoration: "none", fontSize: 18 }}>
         <a
-          href="#contact"
-          onClick={() => handlePageChange('Contact')}
+          onClick={() => { handlePageChange('Contact'); toggleDrawer(false)} }
           className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-          
         >
           Contact
         </a>
+        </Link>
         </button>
       </li>
       </ul>
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,36 +1,37 @@
-import React, { useState } from 'react';
-import Nav from './Header';
-import Home from './pages/Home';
-import Portfolio from './pages/Portfolio';
-import Resume from './pages/Resume';
-import Contact from './pages/Contact';
-import Footer from './Footer';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import backgroundImage from './img/home/background2.jpg';
+import React, { useState } from "react";
+import Nav from "./Header";
+import Home from "./pages/Home";
+import Portfolio from "./pages/Portfolio";
+import Resume from "./pages/Resume";
+import Contact from "./pages/Contact";
+import Footer from "./Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import backgroundImage from "./img/home/background2.jpg";
 
 export default function PortfolioContainer() {
   var background = {
     backgroundImage: `url(${backgroundImage})`,
-    repeat: 'no-repeat',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundAttachment: 'fixed',
-    height: '100%',
-    width: '100vw',
-    zIndex: '-1',
- }
+    repeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundAttachment: "fixed",
+    height: "100%",
+    width: "100vw",
+    zIndex: "-1",
+  };
 
-  const [currentPage, setCurrentPage] = useState('Home');
+  const [currentPage, setCurrentPage] = useState("Home");
 
   const renderPage = () => {
-    if (currentPage === 'Home') {
+    if (currentPage === "Home") {
       return <Home />;
     }
-    if (currentPage === 'Portfolio') {
+    if (currentPage === "Portfolio") {
       return <Portfolio />;
     }
-    if (currentPage === 'Resume') {
+    if (currentPage === "Resume") {
       return <Resume />;
     }
     return <Contact />;
@@ -39,12 +40,19 @@ export default function PortfolioContainer() {
   const handlePageChange = (page) => setCurrentPage(page);
 
   return (
-    <div style={background}>
-      <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
-      <main style={{paddingTop: '160px', height: '100%'}}>
-      {renderPage()}
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <div style={background}>
+        <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+        <main style={{ paddingTop: "160px", height: "100%" }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
